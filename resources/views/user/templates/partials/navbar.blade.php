e677458270a8a7281d353ab8a91b76669f3ded4b
Zaidanikram <95071735+Zaidanikram@users.noreply.github.com>
2023-08-15 15:44:44 +0700

    Update navbar.blade.php

--- resources/views/user/templates/partials/navbar.blade.php
+++ resources/views/user/templates/partials/navbar.blade.php
@@ -1,7 +1,7 @@
 <nav id="navbar" class="navbar order-last order-lg-0">
         <ul>
           <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
-          <li><a class="nav-link scrollto" href="#speakers">Fasilitas Hotel</a></li>
+          <li><a class="nav-link scrollto" href="#speakers">Fasilitas Hotel Zaidan </a></li>
           <li><a class="nav-link scrollto" href="#buy-tickets">Kamar Hotel</a></li>
           <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
           <li><a class="buy-tickets scrollto" href="{{ route('login') }}">Login</a></li>
